c2a61a3710168374dd39d0ca10138cf3a9119acd
Mohamed Ayad <ayad@Haker.Mohamed>
2025-09-13 21:05:05 +0300

    updateing files

---
+++ app/postcss.config.cjs
@@ -0,0 +1,4 @@
+// ~/Desktop/law-firm/app/postcss.config.cjs
+module.exports = {
+  plugins: [require("@tailwindcss/postcss")(), require("autoprefixer")()],
+};
---
+++ strapi/src/api/law-firm/controllers/law-firm.ts
@@ -0,0 +1,7 @@
+/**
+ * law-firm controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::law-firm.law-firm');
